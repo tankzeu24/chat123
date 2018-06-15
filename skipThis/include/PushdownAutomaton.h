d628b3028b4ef90fcdaa2af0012b0e129963f948
tankzeu24 <light787@abv.bg>
2018-06-15 03:17:25 +0300

    sol

---
+++ skipThis/include/PushdownAutomaton.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Rule.h"
+#include "PushdownAutomatonTransition.h"
+#include "PushdownAutomatonConfiguration.h"
+#include "PushdownAutomatonState.h"
+#include "Utility.h"
+#include <set>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class PushdownAutomaton
+{
+public:
+	PushdownAutomaton(const set<Rule> & rules, const Variable & initialVar);
+	PushdownAutomaton();
+	~PushdownAutomaton();
+
+	void addTransition(const PushdownAutomatonTransition & newTransition);
+
+	const vector<PushdownAutomatonTransition> getTransitions() const;
+	const bool wordBelongsToLanguage(string word) const;
+	void print();
+private:
+	vector<PushdownAutomatonTransition> transitions;
+	PushdownAutomatonState initialState;
+	Variable initialVariableOfGrammar;
+};
+
