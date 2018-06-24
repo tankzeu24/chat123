992262ceb504bd9d5c8c138d57dd3b0e594c1248
tankzeu24 <light787@abv.bg>
2018-06-24 16:56:03 +0300

    updated

--- rework/OldVersion.java
+++ rework/OldVersion.java
@@ -2,41 +2,34 @@ package asd;
 
 public class OldVersion {
 
-	
-	
-	//check if service is present in the user PC ,by invkoing the command "service --status-all" if it fails log an error.
-	public static boolean isRunning(String service ,IApplicationHandler logger)
-	{
-		
-		  ArrayList<String> command = new ArrayList<String>();
-	        command.add("service");
-	        command.add("--status-all");
-	        try
-	        {
-	            ShellCommandResult res = ApplicationHandlerUtils.executeShellCommand(true, command); //accepts ArrayList<String> only
+	// check if service is present in the user PC ,by invkoing the command "service
+	// --status-all" if it fails log an error.
+	public static boolean isRunning(String service, IApplicationHandler logger) {
 
-	            if (res.isSuccess())
-	            {
-	                // log the command response
-	                logger.info(res.toString());
+		ArrayList<String> command = new ArrayList<String>();
+		command.add("service");
+		command.add("--status-all");
+		try {
+			ShellCommandResult res = ApplicationHandlerUtils.executeShellCommand(true, command); // accepts
+																									// ArrayList<String>
+																									// only
 
-	                if (res.getOutput().contains(service))
-	                {
-	                    return true;
-	                }
+			if (res.isSuccess()) {
+				// log the command response
+				logger.info(res.toString());
 
-	            }
-	            else
-	            {
-	                
-	                logger.error("Failed to find services. Reason: " + res.toString(), null);
-	            }
-	            
-	        }
-	        catch (IOException e)
-	        {
-	            logger.error("Failed to find services.", e);
-	        }
-		
+				if (res.getOutput().contains(service)) {
+					return true;
+				}
+
+			} else {
+
+				logger.error("Failed to find services. Reason: " + res.toString(), null);
+			}
+
+		} catch (IOException e) {
+			logger.error("Failed to find services.", e);
+		}
+
 	}
 }
